c02f2726fcdcbc9e5878dc6f0fac616c4271d379
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-10-08 17:00:58 +0200

    sequelize link with my db created before

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
+
+require("./models/index");
 
 app.listen(process.env.PORT, () => {
   console.log(`server started ${process.env.PORT}`);
---
+++ models/index.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require("sequelize");
+
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "mysql",
+  logging: false,
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Connected to db ${sequelize.config.database}`);
+  } catch (e) {
+    console.log(e);
+  }
+};
+connectDB();
